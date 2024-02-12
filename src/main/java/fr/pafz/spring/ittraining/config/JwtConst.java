632711b666febefcf0f70e8e523c6397253d9629
Adrien Sauvage <94073350+AdriSauv@users.noreply.github.com>
2024-02-12 15:15:41 +0100

    ajout dune classe Appconfig, jwtconst, jwt validator, jwt provider

---
+++ src/main/java/fr/pafz/spring/ittraining/config/JwtConst.java
@@ -0,0 +1,7 @@
+package fr.pfza.springSecurityFilRouge.config;
+
+public class JwtConst {
+
+    public static final String JWT_SECRET = "jshdfbsqfbsqdjfpoapozpqsjkqjsnnxcbksdp";
+    public static final String JWT_HEADER = "Authorization";
+}
